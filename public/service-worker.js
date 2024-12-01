20bda995d60e9a775b1f369c4785112a35a9b9bc
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-12-01 15:47:39 -0600

    Version 1.0.4

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 importScripts("/idb.js");
 
-const CACHE_NAME = "my-app-cache-v1.0.3"; // Updated cache version
+const CACHE_NAME = "my-app-cache-v1.0.4"; // Updated cache version
 const DB_NAME = "request-queue";
 const STORE_NAME = "requests";
 const DB_VERSION = 2;
